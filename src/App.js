a84d93461ebaa7819cd604786e6934ba0fb2e275
msp1999 <panesarmanpreet7@gmail.com>
2020-09-18 02:11:53 +0530

    compeleted till banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Row from './Row'
 import './App.css';
 import requests from './request'
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="App">
-       <Row title="NETFLIX ORIGNALS" fetchUrl = {requests.fetchNetflixOrignals} />
+      <Banner/>
+       <Row title="NETFLIX ORIGNALS" fetchUrl = {requests.fetchNetflixOrignals} isLargeRow />
        <Row title="Trending Now" fetchUrl = {requests.fetchTrending} />
+       <Row title="Top Rated" fetchUrl = {requests.fetchTopRated} />
+       <Row title="Action Movies" fetchUrl = {requests.fetchActionMovies} />
+       <Row title="Comedy Movies" fetchUrl = {requests.fetchHorrorMovies} />
+       <Row title="Horror Movies" fetchUrl = {requests.fetchDocumentaries} />
+       <Row title="Romance movies" fetchUrl = {requests.fetchRomanceMovies} />
+       <Row title="Documentries" fetchUrl = {requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,34 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import axios from './axios'; 
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-const baseUrl = "https://image.tmdb.org/t/p/original/"
-function Row({title,fetchUrl}) {
-// state = short term memory till page refresh
-const [movies,setMovies] = useState([]);
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl,isLargeRow }) {
+  // state = short term memory till page refresh
+  const [movies, setMovies] = useState([]);
 
-useEffect(() => {
-//if [] are blank it will run only once
-    async function fetchData(){
-        const request = await axios.get(fetchUrl);
-        console.log(request);
-        setMovies(request.data.results);
-        return request;
+  useEffect(() => {
+    //if [] are blank it will run only once
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-},[fetchUrl]);//fetchUrl is now a dependancy as we want to re Render the page when url Changes
+  }, [fetchUrl]); //fetchUrl is now a dependancy as we want to re Render the page when url Changes
 
-console.log(movies);
-return (
-        <div className="row">
-            <h2>{title}</h2>
-        <div className="row_posters">
-            {movies.map(movie => {
-             return <img className="row_poster" src={`${baseUrl}${movie.poster_path}`} alt={movie.name}/>
-            })}
-        </div>
-
-        </div>
-    );
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters" >
+        {movies.map((movie) => {
+          return (
+            <img
+            key={movie.id }
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+             src={`${baseUrl}${isLargeRow ?movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
