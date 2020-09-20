ea09e64ea99149e3d033cf9496bd9993e75d1535
msp1999 <panesarmanpreet7@gmail.com>
2020-09-21 01:19:30 +0530

    completed clone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Row from './Row'
 import './App.css';
-import requests from './request'
-import Banner from './Banner'
+import requests from './request';
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner/>
        <Row title="NETFLIX ORIGNALS" fetchUrl = {requests.fetchNetflixOrignals} isLargeRow />
        <Row title="Trending Now" fetchUrl = {requests.fetchTrending} />
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import axios from "./axios";
 import "./Row.css";
 
+import movieTrailer from "movie-trailer";
+
 const baseUrl = "https://image.tmdb.org/t/p/original/";
-function Row({ title, fetchUrl,isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   // state = short term memory till page refresh
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     //if [] are blank it will run only once
@@ -18,22 +22,45 @@ function Row({ title, fetchUrl,isLargeRow }) {
     fetchData();
   }, [fetchUrl]); //fetchUrl is now a dependancy as we want to re Render the page when url Changes
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVarse: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   console.log(movies);
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row_posters" >
-        {movies.map((movie) => {
-          return (
-            <img
-            key={movie.id }
-              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-             src={`${baseUrl}${isLargeRow ?movie.poster_path : movie.backdrop_path}`}
-              alt={movie.name}
-            />
-          );
-        })}
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt=""
+          />
+        ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
